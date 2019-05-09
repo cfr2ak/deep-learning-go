fce7740ed719884c49e69c76f648ab73a8d43f89
cfr2ak <cfr2ak@gmail.com>
2019-05-09 12:52:10 +0900

    get encoder by name

--- dlgo/encoder/base.py
+++ dlgo/encoder/base.py
@@ -1,11 +1,24 @@
+import importlib
+
+
 class Encoder:
     def name(self):
         raise NotImplementedError()
 
     def encode(self, game_state):
+        """
+        turn a Go board point  into an integer point
+        :param game_state:
+        :return:
+        """
         raise NotImplementedError()
 
     def encode_point(self, point):
+        """
+        turns integer index back into go board point
+        :param point:
+        :return:
+        """
         raise NotImplementedError()
 
     def decode_point_index(self, index):
@@ -16,4 +29,15 @@ class Encoder:
 
     def shape(self):
         raise NotImplementedError()
+
+
+def get_encoder_by_name(name, board_size):
+    if isinstance(board_size, int):
+        board_size = (board_size, board_size)
+    module = importlib.import_module('dlgo.encoder.' + name)
+    constructor = getattr(module, 'create')
+    return constructor(board_size)
+
+
+
 
