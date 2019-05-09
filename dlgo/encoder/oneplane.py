8c535e35a39ff7028f19291ed6827bc7ac1221f3
cfr2ak <cfr2ak@gmail.com>
2019-05-09 13:44:19 +0900

    onpointencoder encode method

---
+++ dlgo/encoder/oneplane.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from dlgo.encoder.base import Encoder
+from dlgo.types import Point
+
+
+class OnePointEncoder(Encoder):
+    def __init__(self, board_size):
+        self.board_width, self.board_height = board_size
+        self.num_planes = 1
+
+    def name(self):
+        return 'oneplane'
+
+    def encode(self, game_state):
+        board_matrix = np.zeros(self.shape())
+        next_player = game_state.next_player
+        for r in range(self.board_height):
+            for c in range(self.board_width):
+                point = Point(row=r + 1, col=c + 1)
+                go_string = game_state.board.get_go_string_on_point(point)
+                if go_string is None:
+                    continue
+                if go_string.color == next_player:
+                    board_matrix[0, r, c] = 1
+                else:
+                    board_matrix[0, r, c] = -1
+
+        return board_matrix
+
+
+
+
+
+
