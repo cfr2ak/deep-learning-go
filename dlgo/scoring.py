b2b1e9cb79e2273d3e405644eee4313cd845ad24
cfr2ak <cfr2ak@gmail.com>
2019-05-12 21:42:42 +0900

    GameResult class

--- dlgo/scoring.py
+++ dlgo/scoring.py
@@ -25,3 +25,24 @@ class Territory:
                 self.dame_points.append(point)
 
 
+class GameResult(namedtuple('GameResult', 'b w komi')):
+    @property
+    def winner(self):
+        if self.b > self.w + self.komi:
+            return Player.black
+        else:
+            return Player.white
+
+    @property
+    def winning_margin(self):
+        w = self.w + self.komi
+        return abs(self.b - w)
+
+    def __str__(self):
+        w = self.w + self.komi
+        if self.b > w:
+            return 'B+%.1f' % (self.b - w)
+        else:
+            return 'W+%.1f' % (w - self.b)
+
+
